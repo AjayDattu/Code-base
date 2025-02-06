875a61297e2ac44863ba05ff787985dcde88e8b7
Ajay Dattu <dattuajay005@gmail.com>
2025-02-06 23:35:13 +0530

    #work configuration

---
+++ Dev/js/Zoom.js
@@ -0,0 +1,12 @@
+let imageZoom = document.getElementById('zoom');
+console.log(imageZoom)
+imageZoom.addEventListener('mousemove',(event)=>{
+ imageZoom.style.setProperty('--display','block')
+ let points = {
+    x: (event.offsetX * 100) / imageZoom.offsetWidth,
+    y: (event.offsetY * 100) / imageZoom.offsetHeight
+ }
+ console.log(points)
+ imageZoom.style.setProperty('--x',points.x + '%');
+ imageZoom.style.setProperty('--y',points.y + '%');
+})
